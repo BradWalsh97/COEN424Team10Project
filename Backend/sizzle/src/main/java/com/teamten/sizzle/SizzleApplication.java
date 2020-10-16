4081826f58498adca7eaf87e24733bfdfd40f124
Dean <deancs1997@gmail.com>
2020-10-16 14:48:53 -0400

    init new spring project

---
+++ Backend/sizzle/src/main/java/com/teamten/sizzle/SizzleApplication.java
@@ -0,0 +1,13 @@
+package com.teamten.sizzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SizzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SizzleApplication.class, args);
+	}
+
+}
---
+++ Backend/sizzle/src/test/java/com/teamten/sizzle/SizzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamten.sizzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SizzleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
