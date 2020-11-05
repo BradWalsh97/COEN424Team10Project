ae23db5f0494fa20215865042273841fecc063e8
Dean <deancs1997@gmail.com>
2020-11-05 17:44:00 -0500

    Show random recipes
    Added new card component to present recipes

---
+++ Frontend/sizzle/src/components/Recipe.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-card>
+    <q-card-section>
+      <div class="text-h3">
+        {{ aRecipe.title }}
+      </div>
+    </q-card-section>
+
+    <q-card-section>
+      <q-img :src="aRecipe.image" style="height: 50%; width: 50%" />
+    </q-card-section>
+
+    <q-card-section>
+      <div v-html="aRecipe.summary" />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "Recipe",
+  props: {
+    aRecipe: {
+      required: true
+    }
+  }
+};
+</script>
--- Frontend/sizzle/src/pages/Index.vue
+++ Frontend/sizzle/src/pages/Index.vue
@@ -1,15 +1,41 @@
 <template>
-  <q-page class="flex flex-center">
-  <div class="text-h1">Sizzle man</div>
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page class="full-width column justify-center">
+    <div class="col self-center" style="padding-left:30%;">
+      <div class="text-h1">Random Recipes</div>
+      <q-btn label="Refresh" @click="getRecipes()" style="width: 100px;" />
+
+      <div class="">
+        <div v-for="recipe in recipes" :key="recipe.image">
+          <div style="padding-top: 1%; width: 50%;">
+            <Recipe v-bind:aRecipe="recipe" />
+          </div>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import Axios from "axios";
+import Recipe from "../components/Recipe.vue";
+
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+  components: {
+    Recipe
+  },
+  data() {
+    return {
+      baseUrl: "http://localhost:8181/api/recipes/random/",
+      recipes: {}
+    };
+  },
+  methods: {
+    getRecipes() {
+      Axios.get(this.baseUrl + 5)
+        .then(res => (this.recipes = res.data))
+        .catch(err => console.log("error during get", err));
+    }
+  }
+};
 </script>
