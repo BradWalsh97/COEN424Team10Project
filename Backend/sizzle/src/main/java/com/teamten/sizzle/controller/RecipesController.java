602d408a2f6296b1e6a38b4c0ed934f824061836
Dean <deancs1997@gmail.com>
2020-11-08 17:50:40 -0500

    Added cross origin to the rest controller just in case

--- Backend/sizzle/src/main/java/com/teamten/sizzle/controller/RecipesController.java
+++ Backend/sizzle/src/main/java/com/teamten/sizzle/controller/RecipesController.java
@@ -5,16 +5,14 @@ import com.teamten.sizzle.service.DefaultRecipesServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/recipes")
 public class RecipesController {
