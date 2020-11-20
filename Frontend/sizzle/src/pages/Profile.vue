5fdd7c7589b35abb47b0a5aa7b59999c1a104563
Dean <deancs1997@gmail.com>
2020-11-20 17:41:26 -0500

    Auto stash before merge of "register-login-pages" and "origin/frontend_work"

---
+++ Frontend/sizzle/src/pages/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page>
+    <div></div>
+  </q-page>
+</template>
+
+<script>
+import Axios from "axios";
+
+export default {
+  name: "Profile",
+  data() {
+    return {
+      baseUrl: "http://localhost:8181/profile/",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
