3dc9cf4634df1c66c8f553d90b718316e8efce60
Dean <deancs1997@gmail.com>
2020-11-20 13:52:25 -0500

    Added another layout for other pages

---
+++ Frontend/sizzle/src/layouts/OutsideLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar class="text-primary bg-white">
+        <q-toolbar-title> Sizzle </q-toolbar-title>
+
+        <!-- <q-btn label="Log out" flat /> -->
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "OutsideLayout",
+
+  data() {
+    return {};
+  },
+};
+</script>
