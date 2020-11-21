a28dc6f1148913a070d7ea4e55757003d5d013cb
Dean <deancs1997@gmail.com>
2020-11-21 16:22:53 -0500

    main layout now creates the pages and keeps them alive too

--- Frontend/sizzle/src/layouts/MainLayout.vue
+++ Frontend/sizzle/src/layouts/MainLayout.vue
@@ -1,68 +1,90 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="header-bar">
-      <q-toolbar class="tb text-white bold" >
-        <div class="full-width row justify-between">
-        <div>
-        <q-toolbar-title class="text-h4"> Sizzle </q-toolbar-title>
-        </div>
-        <div style="display: flex; justify-content: center; align-items: center;">
-          <q-btn unelevated name="search" icon="search" label="Search" />
-          <q-btn unelevated name="home" icon="home" label="Home" to="/" clickable v-ripple />
-          <q-btn unelevated name="cookbooks" icon="book" label="Cookbooks" to="/cookbooks" clickable v-ripple />
-        </div>
+    <div class="">
+      <q-header class="header-bar">
+        <q-toolbar class="text-primary bold">
+          <div class="full-width row justify-between">
+            <div>
+              <q-toolbar-title class="text-h4"> Sizzle </q-toolbar-title>
+            </div>
+            <div
+              style="
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              "
+            >
+              <q-tabs v-model="tab" inline-label class="text-primary">
+                <q-tab name="home" icon="home" label="Home" />
+                <q-tab name="search" icon="search" label="Search" />
+                <q-tab name="cookbook" icon="book" label="Cookbooks" />
+              </q-tabs>
+            </div>
 
-  <div>
-            <q-btn label="Log out" flat color="white" />
+            <div>
+              <q-btn label="Log out" flat color="primary" />
+            </div>
           </div>
-        </div>
-      </q-toolbar>
-    </q-header>
+        </q-toolbar>
+      </q-header>
 
-    
-    <q-page-container>
-      <router-view />
-    </q-page-container>
+      <q-page-container>
+        <q-tab-panels keep-alive v-model="tab">
+          <q-tab-panel name="home">
+            <PageIndex />
+          </q-tab-panel>
 
-    <q-img src="../assets/cookbooks-img.jpg" class="bg-image absolute-top" />
+          <q-tab-panel name="search">
+            <PageIndex />
+          </q-tab-panel>
+
+          <q-tab-panel name="book">
+            <CookBooks />
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-page-container>
+    </div>
+
+    <!-- <q-img src="../assets/cookbooks-img.jpg" class="top" /> -->
   </q-layout>
 </template>
 
 <script>
+import PageIndex from "../pages/Index";
+import CookBooks from "../pages/Cookbooks";
 export default {
   name: "MainLayout",
-
+  components: {
+    PageIndex,
+    CookBooks,
+  },
   data() {
     return {
-      tab: ''
+      tab: "home",
     };
   },
   methods: {
     reach(path) {
-      if (this.$router.currentRoute.fullPath !== path)
-        this.$router.push(path);
-    }
-  }
+      if (this.$router.currentRoute.fullPath !== path) this.$router.push(path);
+    },
+  },
 };
 </script>
 
 <style>
-  .bg-image {
-    height: 100%;
-    z-index: -1;
-    background-image: url('/src/assets/cookbooks-img.jpg');
-    background-repeat: no-repeat;
-    background-size: contain;
-  }
-  .text-h4{
-    text-shadow: 2px 2px rgb(49, 49, 49);
-  }
+.bg-image {
+  height: 100%;
+  z-index: -1;
+  background-image: url("/src/assets/cookbooks-img.jpg");
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+.text-h4 {
+  text-shadow: 2px 2px rgb(49, 49, 49);
+}
 
-  .header-bar {
-    background: transparent !important;
-    border-bottom: 1px solid white;
-  }
-  
-
- 
+.header-bar {
+  background: transparent !important;
+  /* border-bottom: 1px solid white; */
+}
 </style>
