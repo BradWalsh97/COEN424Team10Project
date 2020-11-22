604067b3f2e371366b4964fb8fe7dae78f70d5c8
Brad Walsh <bradley.walsh.97@gmail.com>
2020-11-22 17:32:36 -0500

    updates the scripts

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,6 +1,7 @@
-#!bin/bash
-
-docker build -t sizzle/spring-boot-backend ./Backend/ #Build the Backend
+docker build -t sizzle/spring-boot-backend ./Backend/sizzle/ #Build the Backend
 docker build -t sizzle/quasar_fe ./Frontend/sizzle/   #Build the Frontend
-docker build -t sizzle/nginz_reverse_proxy/ ./nginx/  #Build the reverse proxy
+docker build -t sizzle/nginx_reverse_proxy ./Nginx/  #Build the reverse proxy
+#cd Backend/sizzle
+#mvn spring-boot:build-image -Dspring-boot.build-image.imageName=sizzle/spring-boot_backend
+
 
