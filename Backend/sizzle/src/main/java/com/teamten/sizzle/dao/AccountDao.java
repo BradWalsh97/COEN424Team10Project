74b26f3eb1b60eda9c02fc2f622aa753ba7c6b8b
Dean <deancs1997@gmail.com>
2020-11-25 17:22:27 -0500

    New for custom querries
    fixed a bug in the naming convention which prevented backend from running

--- Backend/sizzle/src/main/java/com/teamten/sizzle/dao/AccountDao.java
+++ Backend/sizzle/src/main/java/com/teamten/sizzle/dao/AccountDao.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountDao extends MongoRepository<Account, String>, CustomAccountDaoInterface {
+public interface AccountDao extends MongoRepository<Account, String>, CustomAccountDao {
     public Account findAccountByUsername(String username);
 
 //    public default void addRecipeToAccount(String username, int recipeId){
--- Backend/sizzle/src/main/java/com/teamten/sizzle/dao/CustomAccountDao.java
+++ Backend/sizzle/src/main/java/com/teamten/sizzle/dao/CustomAccountDao.java
@@ -1,54 +1,11 @@
 package com.teamten.sizzle.dao;
 
-import com.teamten.sizzle.model.Account;
 import com.teamten.sizzle.model.CookBook;
 import com.teamten.sizzle.model.Recipe;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.FindAndModifyOptions;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 
-public class CustomAccountDao implements CustomAccountDaoInterface {
-    private final MongoOperations mongoOperations;
-
-    @Autowired
-    public CustomAccountDao(MongoOperations mongoOperations) {
-        this.mongoOperations = mongoOperations;
-    }
-
-    @Override
-    public void addNewRecipeToUser(String user, int cookBookId, Recipe recipe) {
-        Query query = new org.springframework.data.mongodb.core.query.Query();
-        query.addCriteria(Criteria.where("username").is(user).and("cookBooks").elemMatch(Criteria.where("id").is(cookBookId)));
-        Update update = new Update();
-        update.addToSet("recipeIds", recipe.getId());
-        mongoOperations.findAndModify(query, update, Account.class);
-    }
-
-    @Override
-    public void removeRecipeFromUserAccount() {
-
-    }
-
-    @Override
-    public void addNewCookBook(String user, CookBook cookBook) {
-        Query query = new Query();
-        query.addCriteria(Criteria.where("username").is(user));
-        Update update = new Update();
-        update.addToSet("cookBooks", cookBook);
-        mongoOperations.findAndModify(query, update, Account.class);
-    }
-
-
-    @Override
-    public void removeCookBook(String user, int cookBookId) {
-        Query query = new Query();
-        query.addCriteria(Criteria.where("username").is(user).and("cookBooks").elemMatch(Criteria.where("id").is(cookBookId)));
-        Update update = new Update();
-        update.pull("cookBooks", Criteria.where("id").is(cookBookId));
-        FindAndModifyOptions options = FindAndModifyOptions.options();
-        mongoOperations.findAndModify(query, update, options, Account.class);
-    }
+public interface CustomAccountDao {
+    void addNewRecipeToUser(String user, int cookBookId,Recipe recipe);
+    void removeRecipeFromUserAccount();
+    void addNewCookBook(String user, CookBook cookBook);
+    void removeCookBook(String user, int cookBookId);
 }
