62ade98bb92fcdc34f2ddf36ee25204d1f190fc7
Dean <deancs1997@gmail.com>
2020-11-27 12:12:57 -0500

    making use of vuex

--- Frontend/sizzle/src/store/module-example/mutations.js
+++ Frontend/sizzle/src/store/module-example/mutations.js
@@ -1,2 +1,10 @@
-export function someMutation (/* state */) {
+export function someMutation(/* state */) {}
+
+export function LOGGED_IN(state, id) {
+  state.id = id;
+  state.authenticated = true;
+}
+export function LOGGED_OUT(state) {
+  state.id = "";
+  state.authenticated = false;
 }
