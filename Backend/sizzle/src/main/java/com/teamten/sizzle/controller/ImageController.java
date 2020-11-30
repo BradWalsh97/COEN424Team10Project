4c4fa40e15d3718b774904e014f65a156f4ac8d4
Brad Walsh <bradley.walsh.97@gmail.com>
2020-11-30 02:36:34 -0500

    added endpoints for the different file types

--- Backend/sizzle/src/main/java/com/teamten/sizzle/controller/ImageController.java
+++ Backend/sizzle/src/main/java/com/teamten/sizzle/controller/ImageController.java
@@ -1,22 +1,41 @@
 package com.teamten.sizzle.controller;
 
+import com.teamten.sizzle.model.Tag;
 import com.teamten.sizzle.service.ImageService;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
-@RestController("/image")
+
+@RestController
+@RequestMapping("image")
 public class ImageController {
 
     @Autowired
     private ImageService imageService;
 
-    @PostMapping("/analyse")
-    public String analyseImage(@RequestPart(value = "file") final MultipartFile file) {
+    @PostMapping("/analyseFile")
+    //public String analyseImage(@RequestPart(value = "file") final MultipartFile file) {
+    public List<Tag> analyseImage(@RequestBody final File file){
         return imageService.analyse(file);
     }
+    @PostMapping("/analyseByte")
+    public List<Tag> analyseImage(@RequestBody final byte[] file) throws IOException {
+        File tmpByteFile = new File("tmpByteFile");
+        FileUtils.writeByteArrayToFile(tmpByteFile, file);
+        return imageService.analyse(tmpByteFile);
+    }
+    @PostMapping("/analyseMultiFile")
+    public List<Tag> analyseImage(@RequestBody final MultipartFile file) throws IOException {
+        File tmpFile = new File("image.tmp");
+        file.transferTo(tmpFile);
+        return imageService.analyse(tmpFile);
+    }
+
 
 }
