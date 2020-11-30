139429faede9dad26b888866e24386b07bfdbd1d
Dean <deancs1997@gmail.com>
2020-11-29 23:11:44 -0500

    fixed my mistake and added ms code

--- Backend/sizzle/src/main/java/com/teamten/sizzle/controller/ImageController.java
+++ Backend/sizzle/src/main/java/com/teamten/sizzle/controller/ImageController.java
@@ -5,19 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//From microsoft
-import java.net.URI;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
-
-
 @RestController
 public class ImageController {
 
--- Backend/sizzle/src/main/java/com/teamten/sizzle/service/ImageService.java
+++ Backend/sizzle/src/main/java/com/teamten/sizzle/service/ImageService.java
@@ -1,10 +1,72 @@
 package com.teamten.sizzle.service;
 
-
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
 
 @Service
 public class ImageService {
 
+    private String subscriptionKey = System.getenv("COMPUTER_VISION_SUBSCRIPTION_KEY"); //TODO: put our key
+    private String endpoint = System.getenv("COMPUTER_VISION_ENDPOINT");// TODO: get that
 
+    private final String uriBase = endpoint + "vision/v3.1/analyze";
+    private final String imageToAnalyze =
+            "https://upload.wikimedia.org/wikipedia/commons/" +
+                    "1/12/Broadway_and_Times_Square_by_night.jpg"; //TODO: Get that image
+
+    public void analyse() {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+
+        // Add your Computer Vision subscription key and endpoint to your environment variables.
+        // After setting, close and then re-open your command shell or project for the changes to take effect.
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        try {
+            URIBuilder builder = new URIBuilder(uriBase);
+
+            // Request parameters. All of them are optional.
+            builder.setParameter("visualFeatures", "Categories,Description,Color");
+
+            // Prepare the URI for the REST API method.
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity requestEntity =
+                    new StringEntity("{\"url\":\"" + imageToAnalyze + "\"}");
+            request.setEntity(requestEntity);
+
+            // Call the REST API method and get the response entity.
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("REST Response:\n");
+                System.out.println(json.toString(2));
+            }
+        } catch (Exception e) {
+            // Display error message.
+            System.out.println(e.getMessage());
+        }
+    }
 }
