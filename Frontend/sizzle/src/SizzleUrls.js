ead87e4a917ce13fad35cead3b774da0b2d75de1
abro_ <jjdonofrio@hotmail.com>
2020-11-30 20:23:37 -0500

    merged front_end with azure stuff

--- Frontend/sizzle/src/SizzleUrls.js
+++ Frontend/sizzle/src/SizzleUrls.js
@@ -1,8 +1,6 @@
 //Use for local deployment
-// const urls = {
-//   profileUrl: "http://localhost:8181/profile/",
-//   recipeUrl: "http://localhost:8181/recipes/",
-// };
+ const urls = {   profileUrl: "http://localhost:8181/profile/",
+   recipeUrl: "http://localhost:8181/recipes/", };
 // export default urls;
 
 
@@ -11,8 +9,8 @@
 //   profileUrl: "http://18.188.119.141:8181/profile/",
 //   recipeUrl: "http://18.188.119.141:8181/recipes/",
 // };
-const urls = {
+/*const urls = {
   profileUrl: "http://18.188.119.141:8181/api/profile/",
   recipeUrl: "http://18.188.119.141:8181/api/recipes/",
-};
+};*/
 export default urls;
