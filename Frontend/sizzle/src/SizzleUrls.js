f8592300e5edbc84161dd9ee5895bfe408f23d28
Brad Walsh <bradley.walsh.97@gmail.com>
2020-12-02 22:25:47 -0500

    updated urls for cloud

--- Frontend/sizzle/src/SizzleUrls.js
+++ Frontend/sizzle/src/SizzleUrls.js
@@ -1,19 +1,22 @@
 //Use for local deployment
-const urls = {
-  profileUrl: "http://localhost:8181/profile/",
-  recipeUrl: "http://localhost:8181/recipes/",
-  searchUrl: "http://localhost:8181/search/",
-  imageUrl: "http://localhost:8181/image/",
-};
+// const urls = {
+//   profileUrl: "http://localhost:8181/profile/",
+//   recipeUrl: "http://localhost:8181/recipes/",
+//   searchUrl: "http://localhost:8181/search/",
+//   imageUrl: "http://localhost:8181/image/",
+// };
 // export default urls;
 
 //use for aws deployment
+const urls = {
+  profileUrl: "http://3.131.85.201:80/api/profile/",
+  recipeUrl: "http://3.131.85.201:80/api/recipes/",
+  searchUrl: "http://3.131.85.201:80/api/search/",
+  imageUrl: "http://3.131.85.201:80/api/image/",
+};
 // const urls = {
 //   profileUrl: "http://18.188.119.141:8181/profile/",
 //   recipeUrl: "http://18.188.119.141:8181/recipes/",
 // };
-const urls = {
-  profileUrl: "http://3.131.85.201:80/api/profile/",
-  recipeUrl: "http://3.131.85.201:80/api/recipes/",
-};
+
 export default urls;
